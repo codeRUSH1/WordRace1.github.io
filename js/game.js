f155fe1c122057ad5246094f2406b1e7b3ec590c
codeRUSH1 <60154750+codeRUSH1@users.noreply.github.com>
2020-10-19 09:56:30 +0100

    Prototype 1 game.js

--- js/game.js
+++ js/game.js
@@ -1,12 +1,17 @@
 
-const QuoteApi = 'https://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-const timerElement = document.getElementById('timer')
+const QuoteApi = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+const startButton = document.getElementById('start-btn');
+document.getElementById('counter').innerHTML = '0';
+
+
+
 
 quoteInputElement.addEventListener('input', () => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-  const arrayValue = quoteInputElement.value.split('')
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+  const arrayValue = quoteInputElement.value.split('');
 
   let correct = true
   arrayQuote.forEach((characterSpan, index) => {
@@ -14,18 +19,18 @@ quoteInputElement.addEventListener('input', () => {
     if (character == null) {
       characterSpan.classList.remove('correct')
       characterSpan.classList.remove('incorrect')
-      correct = false
+      correct = false;
     } else if (character === characterSpan.innerText) {
       characterSpan.classList.add('correct')
       characterSpan.classList.remove('incorrect')
     } else {
       characterSpan.classList.remove('correct')
       characterSpan.classList.add('incorrect')
-      correct = false
+      correct = false;
     }
   })
-
-  if (correct) displayNewQuote()
+  if (correct) displayNewQuote() 
+  document.getElementById('counter').innerHTML++;
 })
 
 function getRandomQuote() {
@@ -37,23 +42,44 @@ function getRandomQuote() {
 async function displayNewQuote() {
   const quote = await getRandomQuote()
   quoteDisplayElement.innerHTML = ''
+  counterValue = 0;
+  counterValue++;
   quote.split('').forEach(character => {
+    counterValue = 0;
+    counterValue++;
     const characterSpan = document.createElement('span')
     characterSpan.innerText = character
     quoteDisplayElement.appendChild(characterSpan)
   })
-  quoteInputElement.value = null
-  startTimer()
+  quoteInputElement.value = null;
 }
 
 let startTime
+
 function startTimer() {
-  timerElement.innerText = 0
+  counterValue = 0;
+  counterValue++;
+  timerElement.innerText = 0; 
   startTime = new Date()
   setInterval(() => {
     timer.innerText = getTimerTime()
-  }, 1000)
+  }, 15)
+  quoteInputElement.disabled = false;
+  quoteInputElement.style.visibility = 'visible';
+  quoteDisplayElement.hidden = false;
 }
+
+startButton.addEventListener('click', startTimer); 
+
+// Score counter
+
+
+
+
+// First difficulty level for testing
+
+// Beginner mode: 
+
 
 function getTimerTime() {
   return Math.floor((new Date() - startTime) / 1000)
